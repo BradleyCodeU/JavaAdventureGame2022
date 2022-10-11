8c843663043e41eb9ce6306e9c2b42bd38bb084b
BudderBroHam <37490500+BudderBroHam@users.noreply.github.com>
2022-10-11 08:20:07 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,6 +1,6 @@
 /*
 Item (11 tasks)
- dibs AJ - private instance vars for name, strength, description, int magicType, Random rng
+ dibs AJ - private instance vars for name, strength, description, int magicType, Random rng ✓
  dibs AJ + NoArgsConstructor
  dibs AJ + Item(String _name, String _description)
  dibs AJ + int getMagicType()
@@ -12,23 +12,33 @@ Item (11 tasks)
  dibs WB + toString() // returns the description
  dibs WB + void weaken()
  */
-import java.util.Random;
+
+
 
 public class Item
 {
   // instance variables go here
   // private instance vars for name, strength, description, int magicType, Random rng
-
+  import java.util.Random;
+  private String name;
+  private int strength;
+  private String description;
+  private int magicType;
+  Random rng = new Random();
   
   public Item()
   {
     // set magicType to a random number 1-3 (inclusive)
+    magicType = rng.nextInt(3)+1;
   }
 
 
   public Item(String _name, String _description)
   {
+    name = _name;
+    description = _description;
     // set magicType to 1
+    magicType = 1;
   }
   public String toString(){
     return description.toString();
@@ -37,7 +47,7 @@ public class Item
 
   public int getMagicType()
   {
-    return 0;
+    return magicType;
   }
 
   public String getName()
